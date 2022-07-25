e9983c9d5972dd212b5e3ba0ea9725d63069fe90
steve <909476821@qq.com>
2022-07-25 15:58:22 +0800

    4.1.0.8

--- Framework/TASDK/TraceAnalysisSDK.xcframework/ios-arm64_i386_x86_64-simulator/TraceAnalysisSDK.framework/Headers/TraceAnalysisVersion.h
+++ Framework/TASDK/TraceAnalysisSDK.xcframework/ios-arm64_i386_x86_64-simulator/TraceAnalysisSDK.framework/Headers/TraceAnalysisVersion.h
@@ -5,8 +5,8 @@
 //  Copyright © 2017年 steve. All rights reserved.
 //
 
-#define TASDKVERSION        @"4.1.0.7"
-#define SDKVERSION          @"4107"
+#define TASDKVERSION        @"4.1.0.8"
+#define SDKVERSION          @"4108"
 #define SDKVERSIONBUILD     @"1"
 
-#define TASDKVersionString  @"TASDK_VERSION_4107.1"
+#define TASDKVersionString  @"TASDK_VERSION_4108.1"
